d6e192c5f44d403330ded6930ac86ba00be8a315
vanstee <vanstee@apache.org>
2020-05-28 09:54:24 -0400

    user2 update

--- utils/start_jupyter.sh
+++ utils/start_jupyter.sh
@@ -1,4 +1,5 @@
 
+# User1 setup in $HOME
 [[ -d wmla-learning-path ]] || git https://github.com/IBM/wmla-learning-path.git
 export PATH=/gpfs/software/wmla-p10a117/wmla_anaconda/b0p036a/anaconda/bin:$PATH
 source /gpfs/software/wmla-p10a117/wmla_anaconda/b0p036a/anaconda/envs/powerai162/./etc/profile.d/conda.sh
@@ -11,3 +12,13 @@ git reset --hard origin/master
 ps -ef |  grep -i [j]upyter-notebook.* | grep `whoami` | sed -e "s/ \{1,\}/ /g" | cut -d " " -f2 | xargs -i kill {}
 nohup jupyter notebook --ip=0.0.0.0 --allow-root --port=$1 --no-browser --NotebookApp.token='aicoc' --NotebookApp.password='' > classlog.out &
 
+# User2 setup in $HOME/user2
+
+[[ -d user2/wmla-learning-path ]] || cd user2;git https://github.com/IBM/wmla-learning-path.git
+
+cd ~/user2/wmla-learning-path
+git fetch
+git reset --hard origin/master
+# Stop start jupyter
+nohup jupyter notebook --ip=0.0.0.0 --allow-root --port=$2 --no-browser --NotebookApp.token='aicoc' --NotebookApp.password='' > classlog2.out &
+ 
