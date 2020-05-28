df7ae35be9efd4e1f1c06e094a9f6a6b402c845c
vanstee <vanstee@apache.org>
2020-05-28 10:14:40 -0400

    fix for start jupyter

--- utils/start_jupyter.sh
+++ utils/start_jupyter.sh
@@ -10,7 +10,7 @@ git fetch
 git reset --hard origin/master
 # Stop start jupyter
 ps -ef |  grep -i [j]upyter-notebook.* | grep `whoami` | sed -e "s/ \{1,\}/ /g" | cut -d " " -f2 | xargs -i kill {}
-nohup jupyter notebook --ip=0.0.0.0 --allow-root --port=$1 --no-browser --NotebookApp.token='aicoc' --NotebookApp.password='' > classlog.out &
+nohup jupyter notebook --ip=0.0.0.0 --allow-root --port=$1 --no-browser --NotebookApp.token='aicoc' --NotebookApp.password='' &> classlog.out &
 
 # User2 setup in $HOME/user2
 cd ~
@@ -20,5 +20,5 @@ cd ~/user2/wmla-learning-path
 git fetch
 git reset --hard origin/master
 # Stop start jupyter
-nohup jupyter notebook --ip=0.0.0.0 --allow-root --port=$2 --no-browser --NotebookApp.token='aicoc' --NotebookApp.password='' > classlog2.out &
+nohup jupyter notebook --ip=0.0.0.0 --allow-root --port=$2 --no-browser --NotebookApp.token='aicoc' --NotebookApp.password='' &> classlog2.out &
  
